c7ad7fed890c1b6817f3a0d9c193b417d8f0e7b8
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2015-05-31 18:23:42 -0500

    Change script to by DRYer.
    Extracted name of log file into variable to prevent misspelling errors.

--- install_chef_solo.sh
+++ install_chef_solo.sh
@@ -1,4 +1,4 @@
-wget --output-file=/vagrant/install_chef_solo.log.txt -O chefdk_0.6.0-1.deb https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_0.6.0-1_amd64.deb
-
-cat /vagrant/install_chef_solo.log.txt
+LOG_FILE=/vagrant/install_chef_solo.log.txt
+wget --output-file=$LOG_FILE -O chefdk_0.6.0-1.deb https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_0.6.0-1_amd64.deb
+cat $LOG_FILE
 sudo dpkg -i chefdk_0.6.0-1.deb
