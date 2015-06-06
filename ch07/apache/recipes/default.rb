6821826eaeab98defaacdee0b4fcb30baa6cc2b4
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2015-06-06 17:05:59 -0500

    Configure httpd service.
    Configure httpd service to:
    
    * Start at system start (:start).
    * Automatically enable the system on system restart (:enable).

--- ch07/apache/recipes/default.rb
+++ ch07/apache/recipes/default.rb
@@ -4,4 +4,13 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
+# Install the httpd package.
 package "httpd"
+
+
+# Configure the package to:
+# - Start at system start (:start)
+# - Automatically enable it on system restart (:enable)
+service "httpd" do
+  action [ :enable, :start ]
+end
