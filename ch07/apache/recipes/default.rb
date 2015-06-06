60d1da849f2a2af812a3b95d7b4996024f4a732a
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2015-06-06 17:17:33 -0500

    Provision the home page for the server.
    Change the default recipe to include provisioning the server home page from a template home page.

--- ch07/apache/recipes/default.rb
+++ ch07/apache/recipes/default.rb
@@ -14,3 +14,9 @@ package "httpd"
 service "httpd" do
   action [ :enable, :start ]
 end
+
+# Create a initial home page.
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+  mode '0644'
+end
