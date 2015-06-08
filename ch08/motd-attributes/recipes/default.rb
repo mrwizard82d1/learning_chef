ec55afa40b41718eb61a04b2a6a56ed5dde589f4
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2015-06-07 21:10:55 -0500

    Add debug information to converge run.
    Add a call to `note.debug_value()` to the default recipe. This call
    allows us to see the value of `ipaddress` at run-time.
    
    Note tha the printing **does not** occur as part of the command `kitchen
    login` but only during the call to `kitchen converge.`

--- ch08/motd-attributes/recipes/default.rb
+++ ch08/motd-attributes/recipes/default.rb
@@ -5,12 +5,17 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 
+require 'pp'
+
+
 # Try to override an automatic attribute.
 node.default['ipaddress'] = '1.1.1.1'
+pp node.debug_value('ipaddress')
 
-# Define "recipe-level" attributes.
-node.default['motd-attributes']['company'] = 'cjl-magistri'
-node.default['motd-attributes']['message'] = "It's a wonderful day today!"
+# Define "recipe-level" attributes. 
+node.default['motd-attributes']['company'] = 'cjl-magistri' 
+node.default['motd-attributes']['message'] = 
+	"It's a wonderful day today!"
 
 
 # Include other recipes (which may, in turn, define attributes).
