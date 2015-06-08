2a5c751809f0afba5a9045d75e0f53ea42cfea18
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2015-06-07 20:04:05 -0500

    Create initial attributes.
    Create the initial attribute: ['motd-attributes']['message'].

---
+++ ch08/motd-attributes/attributes/default.rb
@@ -0,0 +1,5 @@
+# By convention, attributes have a top-level key. In this instance, the
+# top-leve is 'motd-attributes'. This "level" contains the 'company'
+# attribute whose value is 'Chef.'
+
+default['motd-attributes']['company'] = 'Chef'
--- ch08/motd-attributes/recipes/default.rb
+++ ch08/motd-attributes/recipes/default.rb
@@ -5,6 +5,10 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 
+# Define an "recipe-level" attribute.
+node.default['motd-attributes']['message'] = "It's a wonderful day today!"
+
+
 template '/etc/motd' do
   source 'motd.erb'
   mode '0644'
